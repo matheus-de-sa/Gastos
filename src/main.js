50813cbe47f9a9ddc7dcef34db680268a495f2f8
Matheus de Sá <78873494+matheus-de-sa@users.noreply.github.com>
2021-05-30 23:45:02 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,16 +8,6 @@ import 'firebase/database'
 import 'firebase/storage'
 import './utils/directives'
 
-Vue.prototype.$firebase = firebase.initializeApp({
-  apiKey: 'AIzaSyCIIQVOVVJtyC9Fg8CCpXkDJbre7YC2gYg',
-  authDomain: 'vue-projeto-b8c20.firebaseapp.com',
-  projectId: 'vue-projeto-b8c20',
-  storageBucket: 'vue-projeto-b8c20.appspot.com',
-  databaseURL: 'https://vue-projeto-b8c20-default-rtdb.firebaseio.com/',
-  messagingSenderId: '183270868613',
-  appId: '1:183270868613:web:cfd6aa8f8b78aaf4274bec'
-})
-
 Vue.config.productionTip = false
 
 new Vue({
